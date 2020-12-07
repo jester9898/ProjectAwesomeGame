b37147db4f612265d07e24a772e79fb9bca837dc
jester9898 <52189526+jester9898@users.noreply.github.com>
2020-12-06 23:57:43 -0500

    final edits

---
+++ ProjectAwesomeness/Assets/Script/Movement/Movement/LimbCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerController playerController;
+
+    private void Start(){
+        playerController= GameObject.FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
+    }
+    private void OnCollisionEnter(Collision collision){
+        playerController.isGrounded = true;
+    }
+}
---
+++ ProjectAwesomeness/Assets/Script/Movement/Movement/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float strafeSpeed;
+    public float jumpForce=16000;
+    public Rigidbody hips;
+    public bool isGrounded;
+
+
+
+    void Start()
+    {
+        hips = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate(){
+        if(Input.GetKey(KeyCode.UpArrow)){
+            hips.AddForce(hips.transform.forward * speed);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            hips.AddForce(-hips.transform.forward * speed);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            hips.AddForce(-hips.transform.right * speed);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            hips.AddForce(hips.transform.right * speed);
+        }
+        if(Input.GetKey(KeyCode.Space)){
+            if(isGrounded){
+                hips.AddForce(new Vector3(0,jumpForce,0));
+                isGrounded=false;
+            }
+            
+        }
+    }
+   
+}
